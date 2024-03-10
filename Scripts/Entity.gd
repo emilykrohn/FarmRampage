ee894b385a73588ed2bba75d4641d33f8eb85f2e
Emily Krohn <emilykrohn2@gmail.com>
2024-03-10 16:43:28 -0700

    Create base Entity Class
    Player and enemy classes will extend this

---
+++ Scripts/Entity.gd
@@ -0,0 +1,18 @@
+extends Node
+
+class_name Entity
+
+# default attack danage and health values
+var attack_damage: int = 2
+var health: int = 10
+
+func attack(target: Entity) -> void:
+	target.take_damage(attack_damage)
+
+func take_damage(amount: int) -> void:
+	health -= amount
+	if health <= 0:
+		die()
+
+func die() -> void:
+	queue_free()
